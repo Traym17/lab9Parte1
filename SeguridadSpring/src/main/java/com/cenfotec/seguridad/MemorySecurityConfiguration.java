3c167d585afd2ed33473000c174755b60c8b0e06
Traym17 <51390112+Traym17@users.noreply.github.com>
2020-11-27 21:02:24 -0600

    Add files via upload

---
+++ SeguridadSpring/src/main/java/com/cenfotec/seguridad/MemorySecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.cenfotec.seguridad;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class MemorySecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws
+	Exception {
+	String encodedPassword = new BCryptPasswordEncoder().encode("tracy");
+
+	auth.inMemoryAuthentication()
+	 .withUser("tracy")
+	 .password(encodedPassword)
+	 .roles("manager", "dev")
+	 .and().passwordEncoder(new BCryptPasswordEncoder());
+	 }
+}
